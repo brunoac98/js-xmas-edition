01f25c6448072a45908cb3e63c91743c0db84066
brunoac98 <89816299+brunoac98@users.noreply.github.com>
2021-09-03 19:48:27 -0300

    Agregado de pruebas
    Se agregan pruebas a las validaciones ya que era lo que solicitaba la tarea de la clase 7

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,15 +1,36 @@
 function probarValidarNombre() {
   console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
+      validarNombre('') === 'El nombre no puede estar vacio.',
       'Validar nombre no validó que el nombre no sea vacío',
   );
 
   console.assert(
       validarNombre(
           '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
+      'No puede tener mas de 50 caracteres',
       'Validar nombre no validó que el nombre sea menor a 50 caracteres',
   );
+
+  console.assert(validarNombre('nombre apellido') === '', 'Fallo validar nombre con un nombre valido');
+
+
+  console.assert(validarNombre('1231231232') === 'El campo nombre solo acepta letras. Formato: Nombre y Apellido.', 'Fallo validacion nombre con numeros.');
 }
 
+
+
+function probarValidarDescripcionRegalo(){
+    console.assert(validarDescripcionRegalo('') === 'La descripcion no puede tener menos de 20 caracteres.', 'Fallo validar regalo');
+    console.assert(validarDescripcionRegalo('111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') === 'No puede tener mas de 100 caracteres.', 'Fallo validacion de regalo por max caracteres.');
+    console.assert(validarDescripcionRegalo('1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') === '', 'Fallo validaciondescripcion con una valida.');
+    console.assert(validarDescripcionRegalo('/////////////////////////////////////') === 'No se pueden usar caracteres especiales en la descripcion.', 'Funcion validar descripcion, fallo validacion caracteres');
+}
+
+function probarValidarCiudad(){
+    console.assert(validarCiudad('') === 'Debes elegir una provincia.', 'No funciono la validacion de provincia.');
+    console.assert(validarCiudad('Ciudad') === '', 'Fallo validarCiudad con una ciudad valida');
+}
+
+probarValidarCiudad();
+probarValidarDescripcionRegalo();
 probarValidarNombre();
